9f4ce433f481fd511d372c7c53a4d211904aafc6
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-05-05 17:15:29 -0400

    changed name of static root

--- storyBot/storyBot/settings.py
+++ storyBot/storyBot/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 
 # REST Framework
