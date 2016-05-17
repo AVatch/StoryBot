13e4c12fc02ed083becfdf1f0925040cb1aacc58
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-05-17 14:37:20 -0400

    added about placeholder and link to it

--- storyBot/stories/urls.py
+++ storyBot/stories/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from .views import BotWebHookHandler, HomePageView, StoryDetailView
 
 # API endpoints
@@ -6,6 +7,9 @@ urlpatterns = [
     url(r'^$',
         HomePageView.as_view(),
         name='home'),
+    
+    url(r'^about$',
+         TemplateView.as_view(template_name='about.html')),
     
     url(r'^stories/(?P<pk>[0-9]+)$',
         StoryDetailView.as_view(),
