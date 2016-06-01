61f174193516b7d91d435723101671478a2af47d
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-06-01 00:06:31 -0400

    turn debug on to troubleshoot

--- storyBot/storyBot/settings.py
+++ storyBot/storyBot/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.thestorybot.com']
 
@@ -106,7 +106,8 @@ DB_CONFIG_PROD = {
     'PORT': os.environ.get("DB_PROD_PORT"),
 }
 
-DB_CONFIG = DB_CONFIG_DEV if DEBUG else DB_CONFIG_PROD
+# DB_CONFIG = DB_CONFIG_DEV if DEBUG else DB_CONFIG_PROD
+DB_CONFIG = DB_CONFIG_PROD
 
 DATABASES = { 'default': DB_CONFIG }
 
